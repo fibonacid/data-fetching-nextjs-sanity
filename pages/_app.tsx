f149b1522dfeb1b3fad7d1802d5cb97671b9a1d8
Lorenzo Rivosecchi <lorenzo.rivosecchi@lacolonia.studio>
2022-11-26 16:51:10 +0100

    add progress bar

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,5 +1,18 @@
-import type { AppProps } from 'next/app'
+import type { AppProps } from "next/app";
+import NProgress from "nprogress";
+import "nprogress/nprogress.css";
+import Router from "next/router";
+
+Router.events.on("routeChangeStart", () => {
+  NProgress.start();
+});
+Router.events.on("routeChangeComplete", () => {
+  NProgress.done();
+});
+Router.events.on("routeChangeError", () => {
+  NProgress.done();
+});
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return <Component {...pageProps} />;
 }
