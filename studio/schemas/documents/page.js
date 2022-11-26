4ba5e8156ba937634150e189f08ba63d2811c26a
Lorenzo Rivosecchi <lorenzo.rivosecchi@lacolonia.studio>
2022-11-26 14:38:33 +0100

    create sanity studio

---
+++ studio/schemas/documents/page.js
@@ -0,0 +1,20 @@
+export default {
+  name: "page",
+  title: "Page",
+  type: "document",
+  fields: [
+    {
+      name: "title",
+      title: "Title",
+      type: "string",
+    },
+    {
+      name: "slug",
+      title: "Slug",
+      type: "slug",
+      options: {
+        source: "title",
+      },
+    },
+  ],
+};
