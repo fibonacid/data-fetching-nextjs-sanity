bfccd9d2e724009e7cb7750363a798c3e07aeee3
Lorenzo Rivosecchi <lorenzo.rivosecchi@lacolonia.studio>
2022-11-26 16:06:22 +0100

    show error message

--- pages/e2e-swr/[slug].tsx
+++ pages/e2e-swr/[slug].tsx
@@ -2,6 +2,7 @@ import sanityClient from "@sanity/client";
 import type { GetStaticPropsContext, InferGetStaticPropsType } from "next";
 import { Page } from "../../types";
 import useSWR from "swr";
+import { useEffect } from "react";
 
 // Configure Sanity client
 const client = sanityClient({
@@ -12,6 +13,9 @@ const client = sanityClient({
 });
 
 async function getPageBySlug(slug: string): Promise<Page | null> {
+  // Add artificial delay to simulate slow network
+  await new Promise((resolve) => setTimeout(resolve, 1000));
+  // Fetch page data from Sanity.
   return client.fetch(
     // Since dataset might be private, we need to check for published documents.
     `*[_type == "page" && slug.current == $slug && !(_id in path("drafts.**"))][0]`,
@@ -65,17 +69,19 @@ export default function CSRPage(
   // Infer page props type from the return value of getServerSideProps
   props: InferGetStaticPropsType<typeof getStaticProps>
 ) {
-  const { data: page, isValidating } = useSWR(
-    props.page.slug.current,
-    getPageBySlug,
-    {
-      fallbackData: props.page,
-    }
-  );
+  // Refetch the page data on the client
+  const {
+    data: page,
+    isValidating,
+    error,
+  } = useSWR(props.page.slug.current, getPageBySlug, {
+    fallbackData: props.page,
+  });
 
   if (page) {
     const generatedAt = new Date(props.timestamp).toLocaleString();
     const updatedAt = new Date(page._updatedAt).toLocaleString();
+
     return (
       <main>
         <h1>{props.page.title}</h1>
@@ -85,8 +91,12 @@ export default function CSRPage(
           <br />
           Generated at: <time>{generatedAt}</time>
           {isValidating && <p>Updating...</p>}
+          {error?.message && <p>Error: {error.message}</p>}
         </footer>
       </main>
     );
+  } else {
+    // This won't happen because we are using fallback: "blocking"
+    return <p>Loading...</p>;
   }
 }
